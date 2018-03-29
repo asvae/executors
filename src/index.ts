e038e0b0de6e221437572d67a732dc494df6fe04
Asva <ontrew@gmail.com>
2018-03-29 15:41:51 +0300

    Minor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import CacheExecutor from './modules/Executors/CacheExecutor'
-import Executor from './modules/Executors/Executor'
-import LadderExecutor from './modules/Executors/LadderExecutor'
-import RepeatExecutor from './modules/Executors/RepeatExecutor'
+import _CacheExecutor from './modules/Executors/CacheExecutor'
+import _Executor from './modules/Executors/Executor'
+import _LadderExecutor from './modules/Executors/LadderExecutor'
+import _RepeatExecutor from './modules/Executors/RepeatExecutor'
 
-export {
-  CacheExecutor,
-  Executor,
-  LadderExecutor,
-  RepeatExecutor,
-}
+export const CacheExecutor = _CacheExecutor
+export const Executor = _Executor
+export const LadderExecutor = _LadderExecutor
+export const RepeatExecutor = _RepeatExecutor
